53e18ffe68c8fe86b2e8370112dd36374c39a978
Susmita <shindesusmita96@gmail.com>
2026-08-19 11:02:36 +0530

    Create 05_t_test.R for t-test calculations
    Added t-test analysis for GDP and Agricultural GVA data.

---
+++ R Concepts/05_t_test.R
@@ -0,0 +1,6 @@
+# Dataset - GVA DATA.xlsx (in datasets folder)
+
+t.test(`GDP_2018-19` , `GDP_2019-20` , paired = TRUE , data = GVA_Data)
+
+t.test(`Agri_GVA_2018-19` , `Agri_GVA_2019-20` , paired=TRUE , data = GVA_Data)
+
